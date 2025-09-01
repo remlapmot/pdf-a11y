af20f1921276f08513460aa2f35d0af4e6c619fb
Tom Palmer <remlapmot@hotmail.com>
2025-09-01 07:07:09 +0100

    Update render.R

--- render.R
+++ render.R
@@ -1,5 +1,10 @@
+# All require tinytex installed
+
 rmarkdown::render('basic-rmd.Rmd')
 
 quarto::quarto_render('basic-qmd.qmd')
 
+# brew install typst
 quarto::quarto_render('typst-example.qmd')
+
+quarto::quarto_render('test-qmd-beamer.qmd')
